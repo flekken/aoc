3ae1e2eaf64c78fe2d06d821f7cfc88c885bae60
Vaszil Ádám <vaszil.adam@fps.hu>
2017-12-01 22:09:27 +0100

    Fix some warning with day 1 problem 1

--- day1/problem1.py
+++ day1/problem1.py
@@ -1,5 +1,5 @@
 # opens file with name of "test.txt"
-sum = 0
+sumOfSameElements = 0
 with open('.//day1//input.txt') as txtInputFile:
     firstChr = txtInputFile.read(1)
     lastChr = firstChr
@@ -8,14 +8,12 @@ with open('.//day1//input.txt') as txtInputFile:
         for ch in line:
             print("char:" + ch + " lastChar:"+lastChr)
             if lastChr == ch:
-                sum = sum + int(lastChr)
+                sumOfSameElements = sumOfSameElements + int(lastChr)
                 print("value increased with: "+ lastChr)
             if ch != "":
                 lastChr = ch
     if firstChr == lastChr:
-        sum = sum + int(lastChr)
+        sumOfSameElements = sumOfSameElements + int(lastChr)
         print("value increased with: "+ lastChr)
-        
-print(sum)
 
-txtInputFile.closed
+print(sumOfSameElements)
