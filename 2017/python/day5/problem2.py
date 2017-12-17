392f8f808e10892a21d195ed59bd4d43b97d6bf2
Vaszil Ádám <vaszil.adam@fps.hu>
2017-12-17 17:51:30 +0100

    Added 2017 day 5 problem 2 in Python

---
+++ 2017/python/day5/problem2.py
@@ -0,0 +1,22 @@
+def read_input():
+    all_lines = []
+    with open('input.txt') as txtInputFile:
+        for line in txtInputFile:
+            all_lines.append(int(line))
+    return all_lines
+
+
+lines = read_input()
+jumps = 0
+index = 0
+while len(lines) > index >= 0:
+    jumps += 1
+    lines[index] += 1
+    if lines[index] - 1 >= 3:
+        lines[index] -= 2
+        index = index + lines[index] + 1
+    else:
+        index = index + lines[index] - 1
+
+
+print(jumps)
